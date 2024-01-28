499f2edd10d7c40486b7e1b9172de7c3648b7100
IsaacLund <71041473+IsaacLund@users.noreply.github.com>
2024-01-28 06:30:03 +0000

    Add files via upload
    CLC project 1

---
+++ CLC_1.c
@@ -0,0 +1,93 @@
+// CST-315-WF900
+// Author: Isaac Lund
+// Group: A5
+// Date: 01/20-27/2024
+
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+# define stockSize 1000
+
+void put(int p);
+int get(int i);
+void consume(int i);
+
+int stock = -1, inp_array[stockSize];
+int theProduce;
+int produce() 
+{
+  return theProduce++;
+}
+void *producer(void *ptr)
+{
+  int i;
+  while(1) {
+    i = produce();
+    put(i);
+  }
+}
+
+void *consumer(void *ptr)
+{
+  int i;
+  while(1) {
+    i = rand() % (theProduce + 1);
+    put(i);
+  }
+}
+
+void put(int p) 
+{
+  pthread_mutex_t lock;
+  pthread_mutex_init(&lock, NULL);
+  pthread_mutex_lock(&lock);
+  
+  if (stock < stockSize) {
+    stock = stock + 1;
+    inp_array[stock] = 1;
+    printf("They stocked %i produce, now have %i Stocked.\n", p, stock);
+  } else {
+    printf("Full Stock.\n");
+    sleep(100);
+  }
+  
+  pthread_mutext_unlock(&lock);
+}
+
+int get(int i)
+{
+  pthread_mutex_t lock;
+  pthread_mutex_init(&lock, NULL);
+  pthread_mutex_lock(&lock);
+  
+  if (stock >= i) {
+      stock = stock - 1;
+      consume(i);
+  } else {
+    printf("Out of Stock.\n");
+    sleep(150);
+  }
+  
+  pthread_mutext_unlock(&lock);
+  
+  return stock;
+}
+
+void consume(int i)
+{
+  printf("They wanted %i now there is %i produce left.\n", i, stock);
+}
+
+int main()
+{
+  pthread_t id_1;
+  pthread_t id_2;
+  pthread_create(&id_1, NULL, producer, NULL);
+  pthread_create(&id_2, NULL, consumer, NULL);
+  
+  pthread_join(id_1, NULL);
+  pthread_join(id_2, NULL);
+  return 0;
+}
